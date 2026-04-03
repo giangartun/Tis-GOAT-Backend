2ec3b40312b1010779678569fd682f79db73dee6
Soto2401 <alecito2ale@gmail.com>
2026-04-02 21:18:58 -0400

    Modificacion de la carpeta api

--- routes/api.php
+++ routes/api.php
@@ -5,15 +5,25 @@ use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\HabilidadController;
 
 Route::prefix('usuario')->group(function () {
-
-    Route::post('/register', [UsuarioController::class, 'register']);
+    // Endpoints de registro y autentificaciion de usuarios (no requiere token de acceso)
+    Route::post('/pre-registro', [UsuarioController::class, 'preRegistro']);
+    Route::get('/verificar-email/{token}', [UsuarioController::class, 'verificarEmail']);
     Route::post('/login', [UsuarioController::class, 'login']);
 
-});
+    // Endpoints para datos de usuario registrado (requiere token de acceso)
+    Route::middleware('auth:sanctum')->group(function () {
+        /*
+        AQUI TENDRIA QUE PONER LOS ENDPOINTS PARA QUE VALIDE 
+        SIEMPRE EL TOKEN DE REGISTRO, SI EN ALGUN CASO TALVES COMO PETICION 
+        DE DATOS DEL SISTEMA U OTROS DE ENTORNO , 
+        SI NO SE  NECESITA DATOS DE SESION NO TIENE SENTIDO PONERLO ACA
+        */
+        Route::get('/habilidades/{id_portafolio}',  [HabilidadController::class, 'index']);
+        Route::post('/habilidades',                 [HabilidadController::class, 'store']);
+        Route::get('/habilidades/{id}',             [HabilidadController::class, 'show']);
+        Route::put('/habilidades/{id}',             [HabilidadController::class, 'update']);
+        Route::delete('/habilidades/{id}',          [HabilidadController::class, 'destroy']);
 
-// Rutas de habilidades (temporalmente sin token)
-Route::get('/habilidades/{id_portafolio}',  [HabilidadController::class, 'index']);
-Route::post('/habilidades',                 [HabilidadController::class, 'store']);
-Route::get('/habilidades/{id}',             [HabilidadController::class, 'show']);
-Route::put('/habilidades/{id}',             [HabilidadController::class, 'update']);
-Route::delete('/habilidades/{id}',          [HabilidadController::class, 'destroy']);
+        Route::post('/logout', [UsuarioController::class, 'logout']);
+    });
+});

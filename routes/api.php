48fb8d9d6543e62af97f1ee5e35249954b24f953
Soto2401 <alecito2ale@gmail.com>
2026-04-02 21:04:07 -0400

    modificacion en la carpeta api.php

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\HabilidadController;
 
 Route::prefix('usuario')->group(function () {
+
     Route::post('/register', [UsuarioController::class, 'register']);
     Route::post('/login', [UsuarioController::class, 'login']);
+
 });
+
+// Rutas de habilidades (temporalmente sin token)
+Route::get('/habilidades/{id_portafolio}',  [HabilidadController::class, 'index']);
+Route::post('/habilidades',                 [HabilidadController::class, 'store']);
+Route::get('/habilidades/{id}',             [HabilidadController::class, 'show']);
+Route::put('/habilidades/{id}',             [HabilidadController::class, 'update']);
+Route::delete('/habilidades/{id}',          [HabilidadController::class, 'destroy']);

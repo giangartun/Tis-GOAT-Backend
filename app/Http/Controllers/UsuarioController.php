fe2c4fa06784d57a11f2d7d35c9c6893408bad67
AdrianVallejosFlores <adrianvallejosflores@gmail.com>
2026-04-23 19:15:40 -0400

    fix: modificacion de verificacion de correo

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -51,9 +51,23 @@ class UsuarioController extends Controller
         Cache::put('token_' . $token, $request->email, now()->addMinutes(5));
 
         // En produccion esto suele, fallar, se espera e para servidores de la UNI ya fucnionen sin problemas
-        // Mail::to($request->email)->send( new VerificacionEmail($token, $request->nombre) );
+        try {
+            Mail::to($request->email)->send(
+                new VerificacionEmail($token, $request->nombre)
+            );
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Error al enviar el correo de verificación.',
+                'error'   => $e->getMessage()
+            ], 500);
+        }
+
+        return response()->json([
+            'message' => 'Revisa tu correo para completar el registro. El enlace expira en 15 minutos.'
+        ], 200);
         
-        // Implementacion temporal usanod N8N para deployado en renderr (Validar si quitar luego de q deployemos en el servidor real)
+        // Implementacion temporal usando N8N para deployado en RENDER (Validar si quitar luego de q deployemos en el servidor real)
+        /*
         Http::timeout(5)
             ->when(app()->environment('local'), fn($http) => $http->withoutVerifying())
             ->post("https://training.intersim.cloud/webhook/bdc3192b-756f-492c-b677-9cf13897e1b9", [
@@ -65,6 +79,7 @@ class UsuarioController extends Controller
         return response()->json([
             'message' => 'Revisa tu correo para completar el registro. El enlace expira en 5 minutos.'
         ], 200);
+        */        
     }
 
     // Valida el token del correo y recién crea el usuario en BD
